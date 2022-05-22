2a9f03385e127bb766bf4f75a4389dfc61f15ff2
tubi55 <tubi55@nate.com>
2022-05-22 11:23:54 +0900

    template등록

---
+++ src/App.js
@@ -0,0 +1,21 @@
+//common
+import Header from './component/common/Header';
+import Footer from './component/common/Footer';
+
+//main
+import Visual from './component/main/Visual';
+import Content from './component/main/Content';
+
+//sub
+import Department from './component/sub/Department';
+import Community from './component/sub/Community';
+import Gallery from './component/sub/Gallery';
+import Youtube from './component/sub/Youtube';
+import Location from './component/sub/Location';
+import Join from './component/sub/Join';
+
+function App() {
+	return <h1>Hello</h1>;
+}
+
+export default App;
