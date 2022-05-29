4d510651b9a3f4224fb92225ee1ef4c1b7b84f6b
tubi55 <tubi55@nate.com>
2022-05-29 09:48:05 +0900

    axio로 데이터 가져와서 멤버소개버화면 구성

--- src/component/sub/Department.js
+++ src/component/sub/Department.js
@@ -1,9 +1,32 @@
 import Layout from '../common/Layout';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+const path = process.env.PUBLIC_URL;
 
 function Department() {
+	const [members, setMembers] = useState([]);
+
+	useEffect(() => {
+		axios.get(`${path}/DB/member.json`).then((json) => {
+			setMembers(json.data.members);
+		});
+	}, []);
+
 	return (
 		<Layout name={'Department'}>
-			<p>Department</p>
+			{members.map((member, idx) => {
+				return (
+					<article key={idx}>
+						<div className='inner'>
+							<div className='pic'>
+								<img src={`${path}/img/${member.pic}`} alt={member.name} />
+							</div>
+							<h2>{member.name}</h2>
+							<p>{member.position}</p>
+						</div>
+					</article>
+				);
+			})}
 		</Layout>
 	);
 }
